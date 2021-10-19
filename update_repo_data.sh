5103ec93ede363a865b532767447f99c5a589632
root <root@debpackrepo01.bitaccess.net>
2021-10-19 21:54:34 +0000

    fixed creation of Packages.gz to work better

--- update_repo_data.sh
+++ update_repo_data.sh
@@ -2,12 +2,16 @@
 #Purpose: create/update Packages.gz for all debian repos
 #
 
+PREFIX=/var/www/html/bitaccess
 
-for i in $(echo /var/www/html/bitaccess/dists/{xenial,focal}/{release/stage{1..5},qa,staging,development}/binary-all);do
+cd $PREFIX || exit
+
+for i in $(echo dists/{xenial,focal}/{release/stage{1..5},qa,staging,development}/binary-all);do
     # make sure the newest file is the Packages.gz file
+    # dpkg-scanpackages dists/qa/binary-all | gzip -c9 >dists/qa/binary-all/Packages.gz
     if ! ls -dlrt $i/* 2>/dev/null|tail -1|grep -q $i/Packages.gz;then
-        echo "Creating $i/Packages.gz"
-        cd $i
-        dpkg-scanpackages . 2>/dev/null| gzip -c9  > Packages.gz
+        echo "Creating $PREFIX/$i/Packages.gz"
+        [ -n "$DEBUG" ] && echo dpkg-scanpackages $i \| gzip -c9 \>$i/Packages.gz && echo
+        [ -z "$DEBUG" ] && dpkg-scanpackages $i 2>/dev/null | gzip -c9 >$i/Packages.gz
     fi
 done
