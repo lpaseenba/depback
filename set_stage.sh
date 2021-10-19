9f4896799c3fdf046596daf6bee0204270db80f5
root <root@debpackrepo01.bitaccess.net>
2021-10-19 19:32:26 +0000

    changed directory structure to something more in line with standards

--- set_stage.sh
+++ set_stage.sh
@@ -5,31 +5,33 @@
 
 
 
-DIRPATH=/var/www/html/bitaccess
-
+DIRPATH=/var/www/html/bitaccess/dists
+OS_CODENAME=(xenial focal)
 STAGES=($(echo {development,qa,staging,release/stage{1..5}}))
 PACKAGES=("docker_cloud_btm" "provisioner")
 
-if false;then
-echo "Staging directories:"
-SPACES="$(printf -- ' %.0s' {1..255})"
-PREFIX=""
-for i in ${!STAGES[*]};do
-    echo "  $PREFIX${STAGES[i]}"
-    [ ! -d ${STAGES[i]} ] && echo "      ERROR: ${STAGES[i]} is not a directory"
-    PREFIX="${SPACES:0:$(($i*2))}\`-> "
-done
-echo
+if [ "$1" == "show " ];then
+    echo "Staging directories:"
+    SPACES="$(printf -- ' %.0s' {1..255})"
+    PREFIX=""
+    for i in ${!STAGES[*]};do
+        for codename in ${OS_CODENAME};do
+            echo "  $PREFIX${DIRPATH}/${codename}/${STAGES[i]}"
+            [ ! -d ${DIRPATH}/$codename/${STAGES[i]} ] && echo "      ERROR: ${DIRPATH}/${codename}/${STAGES[i]} is not a directory"
+            PREFIX="${SPACES:0:$(($i*2))}\`-> "
+        done
+    done
+    echo
 
-echo "Packages:"
-for i in ${!PACKAGES[*]};do
-    echo "  ${PACKAGES[i]}"
-done
-echo
+    echo "Packages:"
+    for i in ${!PACKAGES[*]};do
+        echo "  ${PACKAGES[i]}"
+    done
+    echo
 
-echo "================================================================"
+    echo "================================================================"
+    exit
 fi
-
 
 ################################################################
 #
@@ -38,6 +40,8 @@ DO_COLLECT_VERSIONS(){
     declare -gA PKGSTAGE
     unset LINES
     declare -gA LINES
+    unset MAXVER
+    declare -gA MAXVER
     
     MAXPKGLEN=0
     MAXSTAGELEN=0
@@ -47,31 +51,32 @@ DO_COLLECT_VERSIONS(){
 	PACKAGE=${PACKAGES[$i]}
 	[ $MAXPKGLEN -lt ${#PACKAGE} ] && MAXPKGLEN=${#PACKAGE}
 
-	unset MAXVER
 	# round1, collect info
 	for j in ${!STAGES[*]};do
-	    #[ ! -d $DIRPATH/${STAGES[j]} ] && echo "      ERROR: ${STAGES[j]} is not a directory" && continue
-	    [ ! -d $DIRPATH/${STAGES[j]} ] && continue
-	    STAGE="${STAGES[j]}"
-	    [ $MAXSTAGELEN -lt ${#STAGE} ] && MAXPSTAGELEN=${#STAGE}
+            for codename in ${OS_CODENAME};do
+	        [ ! -d ${DIRPATH}/${codename}/${STAGES[j]}/binary-all ] && echo "      ERROR: ${DIRPATH}/${codename}/${STAGES[j]}/binary-all is not a directory" && exit #continue
+	        #[ ! -d ${DIRPATH}/${codename}/${STAGES[j]}/binary-all ] && continue
+	        STAGE="${STAGES[j]}"
+	        [ $MAXSTAGELEN -lt ${#STAGE} ] && MAXPSTAGELEN=${#STAGE}
 
-	    #Get version of the package
-	    VER="$(dpkg -I $(ls $DIRPATH/$STAGE/$PACKAGE*|sort --version-sort|tail -1)|awk '/Version/{print $2}')"
+	        #Get version of the package
+	        VER="$(dpkg -I $(ls ${DIRPATH}/${codename}/$STAGE/binary-all/$PACKAGE*|sort --version-sort|tail -1)|awk '/Version/{print $2}')"
 
-	    #check if it's the higest we have for this package
-	    MAXVER=$(echo "$MAXVER $VER"|xargs -n1|sort --version-sort|tail -1)
-	    [ $MAXVERLEN -lt ${#VER} ] && MAXVERLEN=${#VER}
+	        #check if it's the higest we have for this package
+	        MAXVER[$PACKAGE]=$(echo "${MAXVER[$PACKAGE]} $VER"|xargs -n1|sort --version-sort|tail -1)
+	        [ $MAXVERLEN -lt ${#VER} ] && MAXVERLEN=${#VER}
 
-	    LINES[${#LINES[*]}]="$PACKAGE,$STAGE,$VER"
-	done
-	
+	        LINES[${#LINES[*]}]="$PACKAGE,$STAGE,$VER"
+	    done
+        done
+
 	#round2 - find max version of the package
 	for j in $(seq 0 $((${#LINES[*]}-1)));do
 	     PACKAGE=$(echo ${LINES[$j]}|cut -d, -f1)
 	     STAGE=$(echo ${LINES[$j]}|cut -d, -f2)
 	     VER=$(echo ${LINES[$j]}|cut -d, -f3)
 	    
-	     if [ "$VER" == "$MAXVER" ];then
+	     if [ "$VER" == "${MAXVER[$PACKAGE]}" ];then
 	 	LATEST="<===="
 	 	PKGSTAGE["$PACKAGE"]="$STAGE $VER"
 	     else
@@ -85,9 +90,11 @@ DO_COLLECT_VERSIONS(){
 ################################################################
 #
 DO_SHOW_VERSIONS(){
-    
-#    for i in ${!PACKAGES[*]};do
-#	PACKAGE=${PACKAGES[$i]}
+
+    for i in ${!PKGSTAGE[*]};do
+	printf "%-${MAXPKGLEN}s:   %${MAXPSTAGELEN}s = %s\n" $i ${PKGSTAGE[$i]}
+    done
+    echo
 
     unset PPACKAGE
     for j in $(seq 0 $((${#LINES[*]}-1)));do
@@ -104,7 +111,7 @@ DO_SHOW_VERSIONS(){
 	fi
 
 	let stage_level++
-	if [ "$VER" == "$MAXVER" ];then
+	if [ "$VER" == "${MAXVER[$PACKAGE]}" ];then
 	    LATEST="<===="
 	else
             LATEST=" "
@@ -114,11 +121,7 @@ DO_SHOW_VERSIONS(){
 	#printf "  %2d-%-${MAXPSTAGELEN}s: %${MAXVERLEN}s %s\n" $stage_level $STAGE $VER "$LATEST"
 	printf "  %-${MAXPSTAGELEN}s: %${MAXVERLEN}s %s\n" $STAGE $VER "$LATEST"
     done
-    echo
-    
-    for i in ${!PKGSTAGE[*]};do
-	printf "%-${MAXPKGLEN}s:   %${MAXPSTAGELEN}s = %s\n" $i ${PKGSTAGE[$i]}
-    done
+
 } # DO_SHOW_VERSIONS
 
 ################################################################
@@ -178,31 +181,34 @@ DO_MENU(){
 DO_CHANGE_STAGE(){
     DIR="$1"  # Increase or Decrease
     WHAT="$2" # package name
+
     PKG_INFO="${PKGSTAGE[$WHAT]}"
     PKG_STAGE=$(echo "$PKG_INFO"|awk '{print $1}')
     PKG_VER=$(echo "$PKG_INFO"|awk '{print $2}')
     unset CMD
     
-    #    echo "DIRPATH=$DIRPATH" >>/tmp/q
-    PKGPATH=$DIRPATH/$PKG_STAGE/$WHAT-$PKG_VER.deb
+    for codename in ${OS_CODENAME};do
+        #    echo "DIRPATH=${DIRPATH}/${codename}" >>/tmp/q
+        PKGPATH=${DIRPATH}/${codename}/$PKG_STAGE/binary-all/$WHAT-$PKG_VER.deb
 
-    for i in ${!STAGES[*]};do
-	[ "${STAGES[i]}" == $PKG_STAGE ] && break
-    done
+        for i in ${!STAGES[*]};do
+	    [ "${STAGES[i]}" == $PKG_STAGE ] && break
+        done
 
-    if [[ "$DIR" =~ ^D ]];then
-	[ $i -ge 1 ] && CMD="rm -fv $PKGPATH $DIRPATH/$PKG_STAGE/Packages.gz" || CMD="# already first level, nothing to do"
-    elif [[ "$DIR" =~ ^I ]];then
-	NEWPKGDIR=$DIRPATH/${STAGES[$(($i+1))]}
-	NEWPKGPATH=$NEWPKGDIR/$WHAT-$PKG_VER.deb
-	if [ $i -lt $((${#STAGES[*]}-1)) ];then
-            CMD=("cp -av $PKGPATH $NEWPKGPATH" "rm -fv $DIRPATH/$PKG_STAGE/Packages.gz $NEWPKGDIR/Packages.gz")
+        if [[ "$DIR" =~ ^D ]];then
+	    [ $i -ge 1 ] && CMD="rm -fv $PKGPATH ${DIRPATH}/${codename}/$PKG_STAGE/binary-all/Packages.gz" || CMD="# already first level, nothing to do"
+        elif [[ "$DIR" =~ ^I ]];then
+	    NEWPKGDIR=${DIRPATH}/${codename}/${STAGES[$(($i+1))]}/binary-all
+	    NEWPKGPATH=$NEWPKGDIR/$WHAT-$PKG_VER.deb
+	    if [ $i -lt $((${#STAGES[*]}-1)) ];then
+                CMD=("cp -av $PKGPATH $NEWPKGPATH" "rm -fv ${DIRPATH}/${codename}/$PKG_STAGE/binary-all/Packages.gz $NEWPKGDIR/Packages.gz")
+            else
+                CMD="# already last level, nothing to do"
+            fi
         else
-            CMD="# already last level, nothing to do"
+	    DEB+="ERRROR, unknown direction:$DIR"
         fi
-    else
-	DEB+="ERRROR, unknown direction:$DIR"
-    fi
+    done
 
     unset RESULT
     unset CMDD
--- update_repo_data.sh
+++ update_repo_data.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+#Purpose: create/update Packages.gz for all debian repos
+#
 
 
-for i in $(echo /var/www/html/bitaccess/{release/stage{1..5},qa,staging,development});do
+for i in $(echo /var/www/html/bitaccess/dists/{xenial,focal}/{release/stage{1..5},qa,staging,development}/binary-all);do
     # make sure the newest file is the Packages.gz file
-    if ! ls -dlrt $i/*|tail -1|grep -q $i/Packages.gz;then
+    if ! ls -dlrt $i/* 2>/dev/null|tail -1|grep -q $i/Packages.gz;then
         echo "Creating $i/Packages.gz"
         cd $i
         dpkg-scanpackages . 2>/dev/null| gzip -c9  > Packages.gz
