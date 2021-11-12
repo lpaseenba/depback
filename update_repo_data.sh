a735395367882e074bc1e9525d55d1552059a7c8
root <root@debpackrepo01.bitaccess.net>
2021-11-12 18:40:59 +0000

    removed "release" from stages, copy anything new uploaded

--- update_repo_data.sh
+++ update_repo_data.sh
@@ -2,11 +2,18 @@
 #Purpose: create/update Packages.gz for all debian repos
 #
 
+#First check if any new package is in upload
+LATEST=$(ls /var/www/html/bitaccess/dists/upload/*|sort --version-sort|tail -1)
+if [ ! -e /var/www/html/bitaccess/dists/development/binary-all/${LATEST##*/} ];then
+    echo "copying $LATEST to /var/www/html/bitaccess/dists/development/"
+    cp -av $LATEST /var/www/html/bitaccess/dists/development/binary-all/
+fi
+
 PREFIX=/var/www/html/bitaccess
 
 cd $PREFIX || exit
 
-for i in $(echo dists/{xenial,focal}/{release/stage{1..5},qa,staging,development}/binary-all);do
+for i in $(echo dists/{xenial,focal}/{stage{1..5},qa,staging,development}/binary-all);do
     # make sure the newest file is the Packages.gz file
     # dpkg-scanpackages dists/qa/binary-all | gzip -c9 >dists/qa/binary-all/Packages.gz
     if ! ls -dlrt $i/* 2>/dev/null|tail -1|grep -q $i/Packages.gz;then
