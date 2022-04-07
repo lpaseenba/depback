b4002bdf35b8d5863d4ea64230e8f1db6b9f8073
root <root@debpackrepo01.bitaccess.net>
2022-04-07 15:47:30 +0000

    added error check/handling

--- update_repo_data.sh
+++ update_repo_data.sh
@@ -16,12 +16,28 @@ PREFIX=/var/www/html/bitaccess
 
 cd $PREFIX || exit
 
+exit_rc=0
+[ -z "$TMPDIR" ] && TMPDIR=$(eval echo ~/tmp)
+[ ! -d "$TMPDIR" ] && mkdir -p $TMPDIR && chmod 700 $TMPDIR
+RESULT=$TMPDIR/result_file.log
+
 for i in $(echo dists/{xenial,focal}/{stage{1..5},qa,staging,development}/binary-all);do
     # make sure the newest file is the Packages.gz file
     # dpkg-scanpackages dists/qa/binary-all | gzip -c9 >dists/qa/binary-all/Packages.gz
     if ! ls -dlrt $i/* 2>/dev/null|tail -1|grep -q $i/Packages.gz;then
         echo "Creating $PREFIX/$i/Packages.gz"
-        [ -n "$DEBUG" ] && echo dpkg-scanpackages $i \| gzip -c9 \>$i/Packages.gz && echo
-        [ -z "$DEBUG" ] && dpkg-scanpackages $i 2>/dev/null | gzip -c9 >$i/Packages.gz
+        if [ -z "$DEBUG" ];then
+            #dpkg-scanpackages $i 2>/dev/null | gzip -c9 >$i/Packages.gz
+            dpkg-scanpackages $i 2>$RESULT| gzip -c9 >$i/Packages.gz
+            save_rc=${PIPESTATUS[0]};[ $exit_rc -lt $save_rc ] && exit_rc=$save_rc
+            if [ $save_rc -ne 0 ];then
+                cat $RESULT
+                echo -e "****************\n**************** FAILURE on line $BASH_SOURCE:$LINENO - dpkg-scanpackages $i\n****************\n"
+                rm -f $i/Packages.gz
+            fi
+        else
+            echo dpkg-scanpackages $i \| gzip -c9 \>$i/Packages.gz && echo
+        fi
     fi
 done
+exit $exit_rc
