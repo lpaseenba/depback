4cfc0bd47c20cb9469d734d29d2a899748309f53
root <root@debpackrepo01.bitaccess.net>
2022-05-04 00:19:03 +0000

    allow non standard named stages

--- update_repo_data.sh
+++ update_repo_data.sh
@@ -2,41 +2,46 @@
 #Purpose: create/update Packages.gz for all debian repos
 #
 
-#First check if any new package is in upload
-for PACKAGE in $((cd /var/www/html/bitaccess/dists/upload/;ls *.deb)|sed 's/\(^[a-zA-Z-]*\)-[0-9]*[-\.]*.*/\1/'|sort -u);do
-    LATEST=$(ls /var/www/html/bitaccess/dists/upload/${PACKAGE}*.deb|sort --version-sort|tail -1)
-    if [ ! -e /var/www/html/bitaccess/dists/development/binary-all/${LATEST##*/} ];then
-        echo "copying $LATEST to /var/www/html/bitaccess/dists/development/"
-        cp -av $LATEST /var/www/html/bitaccess/dists/development/binary-all/
-        rm -f /var/www/html/bitaccess/dists/development/binary-all/Packages.gz
-    fi
-done
-
 PREFIX=/var/www/html/bitaccess
-
 cd $PREFIX || exit
+
+#First check if any new package is in upload
+for i in $(find $PREFIX/dists/upload -type d);do
+    STAGE=${i/*upload\//}
+    [ "$STAGE" == "$i" ] && STAGE=development
+    
+    for PACKAGE in $((cd $i;ls *.deb 2>/dev/null)|sed 's/\(^[a-zA-Z-]*\)-[0-9]*[-\.]*.*/\1/'|sort -u);do
+        LATEST=$(ls $i/${PACKAGE}*.deb|sort --version-sort|tail -1)
+        [ ! -d $PREFIX/dists/$STAGE/binary-all ] && mkdir -p $PREFIX/dists/$STAGE/binary-all
+        if [ ! -e $PREFIX/dists/$STAGE/binary-all/${LATEST##*/} ];then
+            echo "copying $LATEST to $PREFIX/dists/$STAGE/"
+            cp -av $LATEST $PREFIX/dists/$STAGE/binary-all/
+            rm -f $PREFIX/dists/$STAGE/binary-all/Packages.gz
+        fi
+    done
+done
 
 exit_rc=0
 [ -z "$TMPDIR" ] && TMPDIR=$(eval echo ~/tmp)
 [ ! -d "$TMPDIR" ] && mkdir -p $TMPDIR && chmod 700 $TMPDIR
 RESULT=$TMPDIR/result_file.log
 
-for i in $(echo dists/{xenial,focal}/{stage{1..5},qa,staging,development}/binary-all);do
+LEN=$(echo -n "$PREFIX"|wc -c);let LEN++
+for i in $(find $PREFIX/dists/ -type d -name binary-all);do
     # make sure the newest file is the Packages.gz file
-    # dpkg-scanpackages dists/qa/binary-all | gzip -c9 >dists/qa/binary-all/Packages.gz
     if ! ls -dlrt $i/* 2>/dev/null|tail -1|grep -q $i/Packages.gz;then
-        echo "Creating $PREFIX/$i/Packages.gz"
+        echo "Creating $i/Packages.gz"
         if [ -z "$DEBUG" ];then
-            #dpkg-scanpackages $i 2>/dev/null | gzip -c9 >$i/Packages.gz
-            dpkg-scanpackages $i 2>$RESULT| gzip -c9 >$i/Packages.gz
+            (cd $PREFIX;dpkg-scanpackages ${i:$LEN} 2>$RESULT| gzip -c9 >$i/Packages.gz)
             save_rc=${PIPESTATUS[0]};[ $exit_rc -lt $save_rc ] && exit_rc=$save_rc
             if [ $save_rc -ne 0 ];then
                 cat $RESULT
-                echo -e "****************\n**************** FAILURE on line $BASH_SOURCE:$LINENO - dpkg-scanpackages $i\n****************\n"
+                echo -e "****************\n**************** FAILURE on line $BASH_SOURCE:$LINENO - dpkg-scanpackages ${i:$LEN}\n****************\n"
                 rm -f $i/Packages.gz
             fi
         else
-            echo dpkg-scanpackages $i \| gzip -c9 \>$i/Packages.gz && echo
+            echo \(cd $PWD\;dpkg-scanpackages ${i:$LEN} \| gzip -c9 \>$i/Packages.gz\)
+            echo
         fi
     fi
 done
